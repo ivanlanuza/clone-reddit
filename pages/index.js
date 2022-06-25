4705eeb884daa7d79f12399310b4e16af26361e9
Ivan Lanuza <icdlanuza@gmail.com>
2022-06-25 16:51:22 +0800

    Complete Code

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,54 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { getPosts } from "lib/data.js";
+import prisma from "lib/prisma";
+import Posts from "components/Posts";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Home({ posts }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const loading = status === "loading";
+
+  if (loading) {
+    return null;
+  }
+
+  if (session && !session.user.name) {
+    router.push("/setup");
+  }
+
   return (
     <div>
       <Head>
-        <title></title>
-        <meta name='description' content='' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>Reddit Clone</title>
+        <meta name="description" content="A great social network!" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Welcome!</h1>
+      <header className="bg-black text-white h-12 flex pt-3 px-5 pb-2">
+        <p>Reddit clone</p>
+        <p className="grow"></p>
+        <a
+          className="flex-l border px-4 font-bold rounded-full mb-1"
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        >
+          {session ? "logout" : "login"}
+        </a>
+      </header>
+
+      <Posts posts={posts} />
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  let posts = await getPosts(prisma);
+  posts = JSON.parse(JSON.stringify(posts));
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
